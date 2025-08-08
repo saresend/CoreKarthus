09d74543f140a6279d00c86f770647e8c70a7c2d
saresend <saresend@usc.edu>
2025-08-07 18:42:08 -0700

    making some basic things public

--- Sources/CoreKarthus/CoreKarthus.swift
+++ Sources/CoreKarthus/CoreKarthus.swift
@@ -1,2 +1,37 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+
+import Foundation
+
+
+/*
+    A Tool spec that is designed to work with Open AIs tool spec
+ */
+
+
+public struct ToolDescription: Codable {
+    let type: String
+    let name: String
+    let description: String
+    let parameters: ToolParameters
+}
+
+public struct ToolParameters: Codable {
+    let type: String
+    let properties: ToolProperties
+    let required: [String]
+}
+
+public struct ToolProperties: Codable {
+    let sign: ToolPropertyDetail
+}
+
+public struct ToolPropertyDetail: Codable {
+    let type: String
+    let description: String
+
+    public init(type: String, description: String) {
+        self.type = type
+        self.description = description
+    }
+}
+
+
